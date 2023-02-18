ff762b816f3ae9b8ec93b986e812c9dbdf18b0ad
fausto-gnato-eng <faustojcl@gmail.com>
2023-02-18 01:56:32 -0300

    atual

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+# Desenha figuras geométricas: Linha, Reyângulo, Círculo
+# ***************************************************************
 import cv2 as cv
+# Desenha figuras geométricas: Linha, Retângulo, Círculo.
+#***********************************************************************
 import numpy as np
-
-img = cv.imread("/endereço /da/imagem/imagem.extensao")
+# **********************************************************************
+img = cv.imread("/home/takion/Documentos/Pixel/clin2.PNG")
 img2 = np.copy(img)
-
-img2 = cv.line(img,(50,50),(300,50),(0,255,0),3) # Cria linha
-
-img2 = cv.rectangle(img,(200,100),(250,150),(200,0,0),3) # cria retangulo
-
-img2 = cv.circle(img,(100,220),(50),(200,0,0),-1) # cria circulo
-
+#***********************************************************************
+def figuras(img2):
+    img2 = cv.line(img2,(50,50),(300,50),(0,255,0),3) # Cria linha
+    img2 = cv.rectangle(img2,(200,100),(250,150),(200,0,0),3) # retangulo
+    img2 = cv.circle(img2,(100,220),(50),(200,0,0),-1) # circulo
+    return img2
+figuras(img2)
+#************************************************************************
 cv.imshow("Caso Clinico", img2)
-
 cv.waitKey(0)
 cv.destroyAllWindows()
